c2605938d2b1758d75f185d5bef6c802da01ce1a
MKA <MKA@A22PC9>
2024-01-19 20:50:22 +0200

    done creating of advertisement

--- ShopApp/Controllers/AdvertisementsController.cs
+++ ShopApp/Controllers/AdvertisementsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using ShopApp.Data;
+using ShopApp.Data.Entities;
 
 namespace ShopApp.Controllers
 {
@@ -13,7 +15,28 @@ namespace ShopApp.Controllers
         }
         public IActionResult Index()
         {
-            return View(context);
+            var adverts = context.Advertisements
+                .Include(x => x.Category)
+                .Include(x => x.Seller)
+                .Include(x => x.Condition)
+                .ToList();
+            return View(adverts);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Advertisement advertisement)
+        {
+            advertisement.Views = 0;
+            advertisement.SellerId = 1;
+            advertisement.PublicationDate = DateTime.Now;
+            context.Advertisements.Add(advertisement);
+            context.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
     }
 }
